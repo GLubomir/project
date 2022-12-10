ed88daa49b8c1199d640d4877da05754050e8cdb
Lubomir <adamantik2021@gmail.com>
2022-12-10 11:06:01 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// const result = confirm("Are you her?");
+// console.log(result);
+
+// const answer = +prompt('Вам есть 18?', "");
+// console.log(answer + 5);
+
+const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваше фамили?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+console.log(typeof(null));
