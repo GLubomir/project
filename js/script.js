f624c2b9f874bb6f66f3de5a6552cacfa457ea9e
Lubomir <adamantik2021@gmail.com>
2022-12-27 16:27:06 +0200

    array

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const arr = [1, 2, 3, 6, 8];
+const arr = [11, 22, 34, 34, 5, 8];
 arr.sort(compareNum);
 console.log(arr);
 
@@ -10,29 +10,28 @@ function compareNum(a, b) {
 
 
 // arr[99] = 0;
+
 // console.log(arr.length);
 // console.log(arr);
 
-// arr.forEach(function (item, index, arr) {
-// 	console.log(`${index}: ${item} внутри масива ${arr}`);
+// arr.forEach(function (elem, i, arr) {
+// 	console.log(`${i}: ${elem} внутри массива ${arr}`);
 // });
-
-
-
-
-// arr.pop();
-
-// arr.push(10);
-// console.log(arr);
-
-// for (let i = 0; i < arr.length; i++){
-// 	console.log(arr[i]);
-// }
-// for (let value of arr) {
-// 	console.log(value);
-// }
 
 // const str = prompt("", "");
 // const products = str.split(', ');
 // products.sort();
 // console.log(products.join('; '));
+
+// arr.pop();
+// arr.push(10);
+
+// console.log(arr);
+
+// for (let i = 0; i < arr.length; i++){
+// 	console.log(arr[i]);
+// }
+
+// for (let value of arr) {
+// 	console.log(value);
+// }
