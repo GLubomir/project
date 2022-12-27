80672441f653106bead8a65f2c8e18ae4d7cbc42
Lubomir <adamantik2021@gmail.com>
2022-12-27 17:14:21 +0200

    proto

--- js/script.js
+++ js/script.js
@@ -1,94 +1,33 @@
 "use strict";
 
-// let a = 5;
-// let b = a;
+let str = "some";
+let strObj = new String(str);
 
-// b = b + 5;
+// console.log(typeof (str));
+// console.log(typeof (strObj));
 
-// console.log(b);
-// console.log(a);
+console.dir([1, 2, 3])
 
-// const obj = {
-// 	a: 5, 
-// 	b: 1
-// };
+const solider = {
+	health: 400,
+	armor: 100,
+	sayHello: function () {
+		console.log('hello');
+	  }
 
-// const copy = obj; //Ссылка
-
-// copy.a = 10;
-
-// console.log(copy);
-// console.log(obj);
-
-function copy(mainObj) {
-	let objCopy = {};
-	for (let key in mainObj) {
-		objCopy[key] = mainObj[key];
-	}
-	return objCopy;
-}
-
-const numbers = {
-	a: 2, 
-	b: 5,
-	c: {
-		x: 7,
-		y: 4
-	}
-}
-
-const newNumbers = copy(numbers);
-newNumbers.a = 10;
-newNumbers.c.x = 10;
-// console.log(newNumbers)
-// console.log(numbers);
-
-
-const add = {
-	d: 17, 
-	e: 20
-}
-
-
-const clone = Object.assign({}, add);
-clone.d = 20;
-
-// console.log(add);
-// console.log(clone);
-
-const oldArray = ['a', 'b', 'c'];
-const newArray = oldArray.slice();
-
-newArray[1] = 'adasdada';
-console.log(newArray);
-console.log(oldArray);
-
-
-const video = ['youtube', 'vimeo', 'tube'],
-	blogs = ['wordpress', 'livejournal', 'blogger'],
-	internet = [...video, ...blogs];
-console.log(internet);
-
-
-function log(a, b, c) {
-	console.log(a);
-	console.log(b);
-	console.log(c);
-}
-
-const num = [2, 5, 7];
-
-log(...num);
-
-const array = ['a', 'b',];
-
-const newAarray = [...array];
-
-const q = {
-	one: 1,
-	two: 2
 };
 
-const newObj = { ...q };
+const jonh = Object.create(solider);
 
-console.log(newObj);
+
+// const jonh = {
+// 	health: 100
+// };
+
+
+
+// // jonh.__proto__ = solider;
+
+// Object.setPrototypeOf(jonh, solider)
+// // console.log(jonh.armor);
+jonh.sayHello();
