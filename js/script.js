eb04f4f542711a52c09443955ab4f8a3aaf8f301
Lubomir <adamantik2021@gmail.com>
2023-01-17 20:09:31 +0200

    object

--- js/script.js
+++ js/script.js
@@ -1,33 +1,20 @@
 "use strict";
 
-let str = "some";
-let strObj = new String(str);
 
-// console.log(typeof (str));
-// console.log(typeof (strObj));
+const numberOfFilms = +prompt('Cколько фильмов вы уже посмотрели?', '');
 
-console.dir([1, 2, 3])
-
-const solider = {
-	health: 400,
-	armor: 100,
-	sayHello: function () {
-		console.log('hello');
-	  }
-
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
 };
 
-const jonh = Object.create(solider);
-
-
-// const jonh = {
-// 	health: 100
-// };
-
-
-
-// // jonh.__proto__ = solider;
-
-// Object.setPrototypeOf(jonh, solider)
-// // console.log(jonh.armor);
-jonh.sayHello();
+const a = prompt('Один из последних просмотренных фильмов?', '');
+const b = prompt('На сколько оцениваете это фильм?', '');
+const c = prompt('Один из последних просмотренных фильмов?', '');
+const d = prompt('На сколько оцениваете это фильм?', '');
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB)
