a7a80b43ed83afb8252351d6c16bde930d416e39
Lubomir <adamantik2021@gmail.com>
2023-01-18 20:03:47 +0200

    practica2

--- js/script.js
+++ js/script.js
@@ -1,13 +1,46 @@
 "use strict";
 
+/* Задание на урок:
 
-first: for (let i = 0; i < 3; i++){
-	console.log(`First level: ${i}`);
-	for (let j = 0; j < 3; j++){
-		console.log(`Second level: ${j}`);
-		for (let k = 0; k < 3; k++){
-			if (k === 2) break first;
-			console.log(`Three level: ${k}`);
-		}
-	}
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
+const numberOfFilms = +prompt("Cколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+if (personalMovieDB.count < 10) {
+  alert("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+  alert("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+  alert("Вы киноман");
+} else {
+  alert('Произошла ошибка');
 }
+
+for (let i = 0; i < 2; i++) {
+  let a = prompt("Один из последних просмотренных фильмов?", "");
+  let b = prompt("На сколько оцениваете это фильм?", "");
+  if (a != null && a != "" && b != null && b != "" && a.length < 50 ) {
+    personalMovieDB.movies[a] = b;
+  } else {
+    i--;
+  }
+}
+
+console.log(personalMovieDB);
