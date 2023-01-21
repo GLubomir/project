412bb61f2330e9caa50f4cc6e0dd430ea4c22653
Lubomir <adamantik2021@gmail.com>
2023-01-21 13:18:18 +0200

    object

--- js/script.js
+++ js/script.js
@@ -1,12 +1,43 @@
 "use strict";
 
-function learJS(lang, callback) {
-  console.log(`Я учу ${lang}`);
-  callback();
-}
-
-function done() {
-  console.log('Я прошел этот урок');
+const options = {
+  name: 'test',
+  width: 1024,
+  hieght: 1024,
+  colors: {
+    border: 'black',
+    bg: 'red'
+  },
+  makeTest: function () {
+    console.log('test');
   }
+};
 
-learJS('JavaScript', done);
+options.makeTest();
+
+const { border, bg } = options.colors;
+console.log(border);
+console.log(bg);
+
+
+// console.log(options.colors.border);
+
+// delete options.name;
+
+// console.log(options);
+
+// console.log(Object.keys(options.colors).length);
+
+// let counter = 0;
+// for (let key in options) {
+//   if (typeof (options[key]) === 'object') {
+//     for (let i in options[key]) {
+//       console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+//       counter++;
+//     }
+//  }else {
+//     console.log(`Свойство ${key} имеет значение ${options[key]}`);  
+//     counter++;  
+//   }
+// }
+// console.log(counter);
