c6fb38efa5c1e0d1b5590470c9b567e2da207c8d
Lubomir <adamantik2021@gmail.com>
2023-01-21 18:45:58 +0200

    practica4 object

--- js/script.js
+++ js/script.js
@@ -1,62 +1,85 @@
 "use strict";
 
+/* Задание на урок:
 
-// const obj = {
-//   a: 10,
-//   b: 5
-// };
+1) У нас уже есть рабочее приложение, состоящее из отдельных функций. Представьте, что
+перед вами стоит задача переписать его так, чтобы все функции стали методами объекта personalMovieDB
+Такое случается в реальных продуктах при смене технологий или подхода к архитектуре программы
 
-function copy(mainObj) {
-  let objCopy = {};
- 
-  let key;
-  for (key in mainObj) {
-    objCopy[key] = mainObj[key];
-  }
-  return objCopy;
-}
+2) Создать метод toggleVisibleMyDB, который при вызове будет проверять свойство privat. Если оно false - он
+переключает его в true, если true - переключает в false. Протестировать вместе с showMyDB.
 
-const numbers = {
-  a: 2,
-  b: 5,
-  c: {
-    x: 7,
-    y: 4,
+3) В методе writeYourGenres запретить пользователю нажать кнопку "отмена" или оставлять пустую строку. 
+Если он это сделал - возвращать его к этому же вопросу. После того, как все жанры введены - 
+при помощи метода forEach вывести в консоль сообщения в таком виде:
+"Любимый жанр #(номер по порядку, начиная с 1) - это (название из массива)"*/
+
+// start();
+
+const personalMovieDB = {
+  count: 0,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: true,
+  start: function () {
+    this.count = +prompt("Cколько фильмов вы уже посмотрели?", "");
+
+    while (this.count == "" || this.count == null || isNaN(this.count)) {
+      this.count = prompt("Cколько фильмов вы уже посмотрели?", "");
+    }
+  },
+  rememberMyFilm: function () {
+    for (let i = 0; i < 2; i++) {
+      let a = prompt("Один из последних просмотренных фильмов?", "");
+      let b = prompt("На сколько оцениваете это фильм?", "");
+      if (a != null && a != "" && b != null && b != "" && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+      } else {
+        i--;
+      }
+    }
+  },
+  detecPersonalLevel: function () {
+    if (personalMovieDB.count < 10) {
+      alert("Просмотрено довольно мало фильмов");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+      alert("Вы классический зритель");
+    } else if (personalMovieDB.count > 30) {
+      alert("Вы киноман");
+    } else {
+      alert("Произошла ошибка");
+    }
+  },
+  showMyDB:function (hidden) {
+    if (!hidden) {
+      console.log(personalMovieDB);
+    }
+  },
+  writeYourGenres: function () {
+    for (let i = 0; i < 3; i++) {
+      let genre = prompt(`Ваш любимый жанр под номером ${i + 1}`);
+      if (genre === '' || genre === null) {
+        console.log('Вы ввели некорректные данные или не ввели их вовсе');
+        i--;
+      } else {
+        
+        personalMovieDB.genres[i] = genre;      
+      }
+    }
+    personalMovieDB.genres.forEach((item, i) => {
+      console.log(`Любимый жанр ${i + 1} - это ${item}`);
+    });
+  },
+  toggleVisibleMyDB: function() {
+    if (this.privat) {
+      this.privat = false;
+    } else {
+      this.privat = true;
+    }
   }
 };
 
-const newNumbers = copy(numbers);
-
-newNumbers.a = 10;
-newNumbers.c.x = 10;
-
-// console.log(newNumbers);
-// console.log(numbers);
-
-const add = {
-  d: 17,
-  e: 20
-};
-
-const clone = Object.assign({}, add);
-
-clone.d = 20;
-
-// console.log(add);
-// console.log(clone);
-
-const oldArray = ['a', 'b', 'c'];
-const newArray = oldArray.slice();
-
-newArray[1] = 'Hello';
-console.log(newArray);
-console.log(oldArray);
-
-const video = ['youtube', 'vimeo'],
-blogs = ['wordpress', 'livejournal', 'blogger'],
-interner = [...video, ...blogs, 'facebook'];
-
-console.log(interner);
 
 
 
