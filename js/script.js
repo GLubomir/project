330cca35d76d389b5a64628669c139bed867f370
Lubomir <adamantik2021@gmail.com>
2023-01-21 12:44:59 +0200

    callback

--- js/script.js
+++ js/script.js
@@ -1,63 +1,12 @@
 "use strict";
 
-let numberOfFilms;
-
-function start() {
-  numberOfFilms = prompt("Cколько фильмов вы уже посмотрели?", "");
-
-  while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-    numberOfFilms = prompt("Cколько фильмов вы уже посмотрели?", "");
-  }
+function learJS(lang, callback) {
+  console.log(`Я учу ${lang}`);
+  callback();
 }
 
-// start();
-
-const personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: [],
-  privat: false,
-};
-
-function rememberMyFilm() {
-  for (let i = 0; i < 2; i++) {
-    let a = prompt("Один из последних просмотренных фильмов?", "");
-    let b = prompt("На сколько оцениваете это фильм?", "");
-    if (a != null && a != "" && b != null && b != "" && a.length < 50) {
-      personalMovieDB.movies[a] = b;
-    } else {
-      i--;
-    }
-  }
-}
-// rememberMyFilm();
-
-function detecPersonalLevel() {
-  if (personalMovieDB.count < 10) {
-    alert("Просмотрено довольно мало фильмов");
-  } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
-    alert("Вы классический зритель");
-  } else if (personalMovieDB.count > 30) {
-    alert("Вы киноман");
-  } else {
-    alert("Произошла ошибка");
-  }  
+function done() {
+  console.log('Я прошел этот урок');
   }
 
-  // detecPersonalLevel( )
-
-function showMyDB(hidden) {
-  if (!hidden) {
-    console.log(personalMovieDB);
-  }
-}
-
-showMyDB(personalMovieDB.privat);
-
-function writeYourGenres(elem) {
-  for (let i = 0; i < 3; i++){
-    elem.genres.push(prompt(`Ваш любимый жанр под номером ${i + 1}`));
-  }
-}
-writeYourGenres(personalMovieDB);
+learJS('JavaScript', done);
